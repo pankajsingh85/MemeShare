f797f7f1f9aacde09650aa124e1929f5fc1c152c
pankaj singh <pankajsingh8540@gmail.com>
2021-04-16 17:47:54 +0530

    final commit

---
+++ app/src/main/java/com/pankaj/memeshare/MainActivity.java
@@ -0,0 +1,100 @@
+package com.pankaj.memeshare;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+     String curentImgUrl=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadMeme();
+    }
+
+    private void loadMeme() {
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        final TextView textView = (TextView) findViewById(R.id.text);
+        // ...
+
+        // Instantiate the RequestQueue.
+        String url = "https://meme-api.herokuapp.com/gimme";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String url= null;
+                        try {
+                            curentImgUrl = response.getString("url");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        ImageView imageView = (ImageView) findViewById(R.id.memeImageView);
+
+                        Glide.with(getApplicationContext()).load(curentImgUrl).listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+                        }).into(imageView);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+    }
+
+    public void shareMeme(View view) {
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"Hey checkout this cool meme by pankaj singh "+curentImgUrl);
+        Intent chooser=Intent.createChooser(intent,"share this meme using ...");
+        startActivity(chooser);
+    }
+
+    public void nextMeme(View view) {
+        loadMeme();
+    }
+}
